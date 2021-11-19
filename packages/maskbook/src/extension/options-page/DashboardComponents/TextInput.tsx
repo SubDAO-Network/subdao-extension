51a6b4800a057d5e8609d33330a7325e878d049e
mmmkay555 <92655097+mmmkay555@users.noreply.github.com>
2021-11-19 23:39:48 +0800

    refactor: add component

---
+++ packages/maskbook/src/extension/options-page/DashboardComponents/TextInput.tsx
@@ -0,0 +1,40 @@
+import { TextField, TextFieldProps, makeStyles } from '@material-ui/core'
+import classNames from 'classnames'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& label': {
+            transform: 'translate(-0, -6px) scale(0.75) !important',
+            textAlign: 'left',
+            position: 'inherit',
+        },
+        '& label text': {
+            color: '#D51172',
+        },
+        '& fieldset': {
+            top: 0,
+        },
+        '& .MuiInputBase-input': {
+            paddingTop: '12px',
+            paddingBottom: '12px',
+        },
+        '& legend': {
+            display: 'none',
+        },
+    },
+}))
+
+export default function TextInput(_props: TextFieldProps) {
+    const { className, ...props } = _props
+    const classes = useStyles()
+    return (
+        <TextField
+            {...props}
+            className={classNames(className, classes.root)}
+            InputLabelProps={{
+                shrink: true,
+                disableAnimation: true,
+            }}
+        />
+    )
+}
