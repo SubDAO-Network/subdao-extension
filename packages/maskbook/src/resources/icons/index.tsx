311fb2ed728dd56ee7aeb96e3814a085c7e0038a
srdothunter <77256808+srdothunter@users.noreply.github.com>
2021-11-22 16:42:57 +0800

    feat: contacts

--- packages/maskbook/src/resources/icons/index.tsx
+++ packages/maskbook/src/resources/icons/index.tsx
@@ -1,4 +1,5 @@
-export type IconsTypes = 'feedback'
+export type IconsTypes = 'feedback' | 'delete'
 export const IconsURLs: Readonly<Record<IconsTypes, { image: string; text: string }>> = {
     feedback: { image: new URL('./feedback.png', import.meta.url).toString(), text: 'feedback' },
+    delete: { image: new URL('./delete.png', import.meta.url).toString(), text: 'delete' },
 }
