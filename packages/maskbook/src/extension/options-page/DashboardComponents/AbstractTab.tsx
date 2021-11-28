810784f1dfb026bd9d5a204739f9e612801c403b
mmmkay555 <92655097+mmmkay555@users.noreply.github.com>
2021-11-28 12:41:01 +0800

    feat: add injectAbstractTab

--- packages/maskbook/src/extension/options-page/DashboardComponents/AbstractTab.tsx
+++ packages/maskbook/src/extension/options-page/DashboardComponents/AbstractTab.tsx
@@ -79,3 +79,70 @@ export default function AbstractTab({ tabs, state, height = 200 }: AbstractTabPr
         </>
     )
 }
+
+const useInjectStyles = makeStyles((theme: Theme) => {
+    return createStyles({
+        tab: {
+            minWidth: 'unset',
+            marginRight: 30,
+            fontSize: 16,
+        },
+        tabSelected: {
+            fontSize: 20,
+        },
+        indicator: {
+            width: 40,
+        },
+        tabPanel: {
+            marginTop: theme.spacing(3),
+            display: 'flex',
+            flexDirection: 'column',
+            height: 230,
+            overflowY: 'auto',
+            // background: '#F7F8FB',
+            '&::-webkit-scrollbar': {
+                display: 'none',
+            },
+        },
+    })
+})
+export function InjectAbstractTab({ tabs, state, height = 200 }: AbstractTabProps) {
+    const classes = useInjectStyles()
+    const [value, setValue] = state
+    // const tabIndicatorStyle = tabs.length ? { width: 100 / tabs.length + '%', height: 2 } : undefined
+
+    return (
+        <>
+            <Paper elevation={0}>
+                <Tabs
+                    value={value}
+                    classes={{ indicator: classes.indicator }}
+                    // TabIndicatorProps={{ style: tabIndicatorStyle }}
+                    // variant="fullWidth"
+                    indicatorColor="primary"
+                    textColor="primary"
+                    onChange={(_: React.SyntheticEvent, newValue: number) => setValue(newValue)}>
+                    {tabs.map((tab) => (
+                        <Tab
+                            className={classes.tab}
+                            classes={{
+                                selected: classes.tabSelected,
+                            }}
+                            label={tab.label}
+                            key={tab.label}
+                            data-testid={`${tab.id?.toLowerCase()}_tab`}
+                        />
+                    ))}
+                </Tabs>
+            </Paper>
+            <Box
+                className={classes.tabPanel}
+                role="tabpanel"
+                {...tabs.find((_, index) => index === value)}
+                sx={{
+                    minHeight: height,
+                }}
+            />
+        </>
+    )
+}
